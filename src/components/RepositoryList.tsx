6368fb4eb4216bb2c0446fe2b817539b12d5245b
marcospsw <marcospsw96@gmail.com>
2021-03-10 17:31:58 -0300

    Fix: Modificações Prettier

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -13,15 +13,15 @@ export function RepositoryList() {
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(reposnse => reposnse.json())
-      .then(data => setRepositories(data));
+      .then((reposnse) => reposnse.json())
+      .then((data) => setRepositories(data));
   }, []);
 
   return (
     <section className="repository-list">
       <h1>Lista de Repositorios</h1>
       <ul>
-        {repositories.map(repository => {
+        {repositories.map((repository) => {
           return (
             <RepositoryItem key={repository.name} repository={repository} />
           );
